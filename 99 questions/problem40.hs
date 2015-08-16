72dbc4040b9f025812d91f23943e346f0d0b9d19
Ward Schodts <wardschodts@hotmail.com>
2015-08-17 00:24:49 +0200

    problem 40

--- 99 questions/problem40.hs
+++ 99 questions/problem40.hs
@@ -1,4 +1,5 @@
 import Control.Monad
+import Data.List
 
 isPrime :: Int -> Bool
 isPrime n = length ([ x | x <- [1..n], n `mod` x == 0]) == 2
@@ -6,14 +7,15 @@ isPrime n = length ([ x | x <- [1..n], n `mod` x == 0]) == 2
 goldbach :: Int -> [(Int,Int)]
 goldbach n = [(x,y)| x <- [1..n], y <- map (n-) [1..n] , isPrime x, isPrime y, x + y == n]
 
---goldbachM :: Int -> [(Int,Int)]
+goldbachM :: Int -> [(Int,Int)]
 goldbachM n = do
         x <- [1..n]
-        y <- [1..n `div` 2]
+        y <- [1..n `div` 2] \\ [x]
+        guard (x + y == n)
         guard (isPrime x)
         guard (isPrime y)
-        guard (x + y == n)
         return (x,y)
+           
         
 
 
