56b6737c6e818e5daf4de1c0a377fa9cf2dd8bc6
Ward Schodts <wardschodts@hotmail.com>
2015-08-20 17:25:21 +0200

    some graphs

--- grafiek.hs
+++ grafiek.hs
@@ -2,10 +2,12 @@ import qualified Graphics.Gnuplot.Frame.OptionSet as Opts
 import Graphics.Histogram 
 
 input = take (3600*4) (cycle [1..400]) ++ take  (3600*4) (cycle [1..40]) ++ take  (3600*4) (cycle [200..500])
+input2 = take 100 [1..]
  
 simple = do
-    let hist = histogram binSturges input
-    plot "simple.png" hist
+    let hist = histogram binSturges input2
+    print (binSturges input)
+    plot "simple2.png" hist
 
 advanced = do
     let hist = histogram binSqrt input
